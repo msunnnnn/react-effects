101ce9f66dfa85b65ab44578e8d6c00f440e0c87
Margaret Sun <hi.margaret.sun@gmail.com>
2022-09-06 12:07:33 -0700

    it might work

--- src/Deck.js
+++ src/Deck.js
@@ -1,25 +1,71 @@
-import React, {useState} from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 
-const CARD_DECK_URL = "https://deckofcardsapi.com/api/deck/new/draw/?count=1"
+const CARD_DECK_URL = "https://deckofcardsapi.com/api/deck/";
 
 
-function Deck(){
+function Deck() {
 
-  const [card, setCard] = useState("");
+  const [deck, setDeck] = useState({
+    id: "",
+    card: "",
+    isLoading: true,
+    remaining: "",
+    error: null
+  });
 
-  async function getCard(){
-    const response = await axios.get(`${CARD_DECK_URL}`);
-    const randomCard = response.data.cards[0]
-    setCard(randomCard)
+  useEffect(function getDeckIdWhenMounted() {
+    async function getDeckId() {
+      const response = await axios.get(`${CARD_DECK_URL}new/`);
+      const deckId = response.data.deck_id;
+      const remainingCards = response.data.remaining;
+      setDeck({
+        id: deckId,
+        card: "",
+        isLoading: false,
+        remaining: remainingCards
+      });
+    }
+    getDeckId();
+  }, []);
+
+
+  async function getCard() {
+    try {
+      const response = await axios.get(`${CARD_DECK_URL}${deck.id}/draw/?count=1`);
+      const randomCard = response.data.cards[0];
+      const remainingCards = response.data.remaining;
+      console.log("remaining cards:", remainingCards);
+      setDeck(deck => (
+        {
+          ...deck,
+          card: randomCard,
+          remaining: remainingCards
+        }));
+    } catch (err) {
+      setDeck(deck => (
+        {
+          ...deck,
+          error: err
+        }));
+      console.log("no more cards")
+    }
+  }
+  if (deck.remaining === 1){
+    console.log("error", deck.error)
+    return (
+
+      <h1>No More Cards</h1>
+
+    )
   }
 
   return (
     <div>
       <button onClick={getCard}>Get Card Using handler</button>
-      <img src= {card.image} alt={card.code} />
+      <img src={deck.card.image} alt={deck.card.code} />
     </div>
-  )
+  );
 
 }
 export default Deck;
